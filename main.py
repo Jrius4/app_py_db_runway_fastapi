5f6900f644301262bc8c7bdba500184cf46c5ab1
Julius Jr <kazibwejuliusjunior@gmail.com>
2021-10-29 05:11:20 +0300

    routing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,7 +9,26 @@ def index():
     return {"data":{'name':'kazibwe'}}
 
 
-@app.get("/about")
+@app.get("/users")
 
-def about():
-    return {"data":'about page'} 
+def get_users(limit:int = 10,sort:Optional[str]=None):
+    result = f'{limit} users from db'
+    return {"data":result} 
+
+
+@app.get("/users/administrator")
+
+def get_users_admins():
+    return {"data":"all administrators"}
+
+
+@app.get("/users/{id}")
+
+def get_user(id:int):
+    return {"data":id} 
+
+@app.post("/users")
+def create_user():
+    return {
+        "data":"User is created"
+    }
